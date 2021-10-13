8b0d1b709e92d420ad3143231e5f947a709f9cf2
josiasveras <josias_veras@hotmail.com>
2021-10-13 11:10:27 -0300

    Project created

---
+++ src/test/java/com/verasmobile/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verasmobile.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
