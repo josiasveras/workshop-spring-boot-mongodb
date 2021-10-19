51f06066aaefe1e1c2a95dde57a0b8b97603b1e6
josiasveras <josias_veras@hotmail.com>
2021-10-19 11:04:38 -0300

    Find user by id

--- src/main/java/com/verasmobile/workshopmongo/services/UserService.java
+++ src/main/java/com/verasmobile/workshopmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.verasmobile.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.verasmobile.workshopmongo.domain.User;
 import com.verasmobile.workshopmongo.repository.UserRepository;
+import com.verasmobile.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
 }
