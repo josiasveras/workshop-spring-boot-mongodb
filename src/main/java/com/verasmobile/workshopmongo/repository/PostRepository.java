eda79a9b287fdc4eb45ea8a096a384125c47c281
josiasveras <josias_veras@hotmail.com>
2021-10-25 11:10:06 -0300

    Entity post

---
+++ src/main/java/com/verasmobile/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.verasmobile.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.verasmobile.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+}
