1acde1a50dd77f05d37a79d4fa0389e190661bbd
josiasveras <josias_veras@hotmail.com>
2021-10-26 11:51:42 -0300

    Add comments to posts

--- src/main/java/com/verasmobile/workshopmongo/config/Instantiation.java
+++ src/main/java/com/verasmobile/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.verasmobile.workshopmongo.domain.Post;
 import com.verasmobile.workshopmongo.domain.User;
 import com.verasmobile.workshopmongo.dto.AuthorDTO;
+import com.verasmobile.workshopmongo.dto.CommentDTO;
 import com.verasmobile.workshopmongo.repository.PostRepository;
 import com.verasmobile.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2021"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem!", sdf.parse("21/03/2021"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2021"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2021"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
