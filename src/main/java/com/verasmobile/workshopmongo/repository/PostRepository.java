5a3f66991f79c7aed1cde908131614e5e0017a41
josiasveras <josias_veras@hotmail.com>
2021-10-27 11:09:59 -0300

    Simple search with @Query

--- src/main/java/com/verasmobile/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/verasmobile/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,17 @@ package com.verasmobile.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.verasmobile.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//Alternative @Query $regex search method
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
